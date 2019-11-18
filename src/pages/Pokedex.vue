43bab7f61c42762f264e19228334bf21f99a7308
Brandon Lyons <brandonl.4231@gmail.com>
2019-11-18 18:56:49 -0500

    Add basic styling to pokedex page

--- src/pages/Pokedex.vue
+++ src/pages/Pokedex.vue
@@ -1,14 +1,17 @@
 <template>
   <Layout>
-    <form @submit.prevent="fetchPokemon(searchTerm)">
-      <input type="text" v-model="searchTerm" placeholder="pikachu" />
-      <button type="submit">Search</button>
-    </form>
+    <section>
+      <form @submit.prevent="fetchPokemon(searchTerm)">
+        <input id="search-input" type="text" v-model="searchTerm" placeholder="pikachu" />
+        <button type="submit">Search</button>
+        <label class="search-label" for="search-input">Try searching for "Pikachu" or "Mewtwo"</label>
+      </form>
 
-    <div class="pokemon-card--container">
-      <h2 class="pokemon-card--title">{{pokemon.name}}</h2>
-      <img v-if="pokemon.imageUrl" :src="pokemon.imageUrl" />
-    </div>
+      <div class="pokemon-card--container">
+        <h2 class="pokemon-card--title">{{capitalizeFirstLetter(pokemon.name)}}</h2>
+        <img v-if="pokemon.imageUrl" :src="pokemon.imageUrl" :alt="`A picture of ${pokemon.name}`" />
+      </div>
+    </section>
   </Layout>
 </template>
 
@@ -36,14 +39,44 @@ export default {
       } catch (err) {
         console.log(err)
       }
+    },
+    capitalizeFirstLetter(string = '') {
+      return `${string.substring(0, 1).toUpperCase()}${string.substring(1)}`
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.search-label {
+  display: block;
+  font-size: 0.8em;
+}
+form {
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  padding: 1em;
+}
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .pokemon-card--container {
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  padding: 1em;
+  margin-top: 1em;
+  min-height: 200px;
+  min-width: 100px;
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+.pokemon-card--container:hover {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 }
 .pokemon-card--title {
+  margin-top: 0;
 }
 </style>
